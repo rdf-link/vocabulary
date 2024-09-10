7c550f96a4ea0c67e4afdebeddd1407ac46e1b32
Matthieu Bosquet <matthieubosquet@gmail.com>
2024-09-10 21:03:15 +0100

    Basic NS tests

---
+++ test/ns.test.ts
@@ -0,0 +1,26 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+import { NS } from "../dist/mod.js";
+
+describe("NS", async () => {
+    it("has core RDF vocabulary", () => {
+        assert.equal(NS.RDF.iri, "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        assert.equal(NS.RDF.prefix, "rdf");
+    })
+
+    it("has core RDFS vocabulary", () => {
+        assert.equal(NS.RDFS.iri, "http://www.w3.org/2000/01/rdf-schema#");
+        assert.equal(NS.RDFS.prefix, "rdfs");
+    })
+
+    it("has core OWL vocabulary", () => {
+        assert.equal(NS.OWL.iri, "http://www.w3.org/2002/07/owl#");
+        assert.equal(NS.OWL.prefix, "owl");
+    })
+
+    it("has core XSD vocabulary", () => {
+        assert.equal(NS.XSD.iri, "http://www.w3.org/2001/XMLSchema#");
+        assert.equal(NS.XSD.prefix, "xsd");
+    })
+})
