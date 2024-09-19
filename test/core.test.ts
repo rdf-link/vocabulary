090823f939623d3f2e54067915ae1fea4b5a7959
Matthieu Bosquet <matthieubosquet@gmail.com>
2024-09-19 12:52:13 +0100

    Refactor tests

---
+++ test/core.test.ts
@@ -0,0 +1,22 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+import { RDF, RDFS, OWL, XSD } from "@rdf-link/vocabulary";
+
+describe("Core vocabularies", async () => {
+    it("RDF has 23 terms", () => {
+        assert.equal(Object.keys(RDF).length, 23);
+    })
+
+    it("RDFS has 15 terms", () => {
+        assert.equal(Object.keys(RDFS).length, 15);
+    })
+
+    it("OWL has 77 terms", () => {
+        assert.equal(Object.keys(OWL).length, 77);
+    })
+
+    it("XSD has 39 terms", () => {
+        assert.equal(Object.keys(XSD).length, 39);
+    })
+})
--- test/ns.test.ts
+++ test/ns.test.ts
@@ -1,31 +1,26 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
 
-import { NS } from "../dist/mod.js";
-import * as VOCAB from "../dist/core/mod.js"
+import { NS } from "@rdf-link/vocabulary";
 
 describe("NS", async () => {
     it("has core RDF vocabulary", () => {
         assert.equal(NS.RDF.iri, "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
         assert.equal(NS.RDF.prefix, "rdf");
-        assert.equal(Object.keys(VOCAB.RDF).length, 23);
     })
 
     it("has core RDFS vocabulary", () => {
         assert.equal(NS.RDFS.iri, "http://www.w3.org/2000/01/rdf-schema#");
         assert.equal(NS.RDFS.prefix, "rdfs");
-        assert.equal(Object.keys(VOCAB.RDFS).length, 15);
     })
 
     it("has core OWL vocabulary", () => {
         assert.equal(NS.OWL.iri, "http://www.w3.org/2002/07/owl#");
         assert.equal(NS.OWL.prefix, "owl");
-        assert.equal(Object.keys(VOCAB.OWL).length, 77);
     })
 
     it("has core XSD vocabulary", () => {
         assert.equal(NS.XSD.iri, "http://www.w3.org/2001/XMLSchema#");
         assert.equal(NS.XSD.prefix, "xsd");
-        assert.equal(Object.keys(VOCAB.XSD).length, 39);
     })
 })
