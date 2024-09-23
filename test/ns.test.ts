5ac33fc563cecdd3419b86d2e8d189cbc69c2289
Matthieu Bosquet <matthieubosquet@gmail.com>
2024-09-23 18:10:17 +0100

    Test namespace terms

--- test/ns.test.ts
+++ test/ns.test.ts
@@ -7,20 +7,24 @@ describe("NS", async () => {
     it("has core RDF vocabulary", () => {
         assert.equal(NS.RDF.iri, "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
         assert.equal(NS.RDF.prefix, "rdf");
+        assert.equal(Object.keys(NS.RDF.terms).length, 23);
     })
 
     it("has core RDFS vocabulary", () => {
         assert.equal(NS.RDFS.iri, "http://www.w3.org/2000/01/rdf-schema#");
         assert.equal(NS.RDFS.prefix, "rdfs");
+        assert.equal(Object.keys(NS.RDFS.terms).length, 15);
     })
 
     it("has core OWL vocabulary", () => {
         assert.equal(NS.OWL.iri, "http://www.w3.org/2002/07/owl#");
         assert.equal(NS.OWL.prefix, "owl");
+        assert.equal(Object.keys(NS.OWL.terms).length, 77);
     })
 
     it("has core XSD vocabulary", () => {
         assert.equal(NS.XSD.iri, "http://www.w3.org/2001/XMLSchema#");
         assert.equal(NS.XSD.prefix, "xsd");
+        assert.equal(Object.keys(NS.XSD.terms).length, 39);
     })
 })
